b30fecbd3cfcc48f50a9d30205e436714c8ecbaf
net9keep <net9keep@naver.com>
2017-08-17 13:09:01 +0900

    [updata] Server updata

--- Server.py
+++ Server.py
@@ -31,25 +31,44 @@ SERVER_PORT = 30001
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((SERVER_IP,SERVER_PORT))
-s.listen()
+s.listen(10)
 input_list = [s]
 
-while True:
-    input_ready, write_ready, except_ready = select.select(input_list, [], [])
+while input_list:
+    try:
+        input_ready, write_ready, except_ready = select.select(input_list, [], [])
 
-    for cl in input_ready:
-        if cl == s:
-            client, address = s.accept()
-            #접속을 허가 했을때 표시할 내용
-            print()
-            input_list.append(client)
-        else:
-            data = cl.recv(1024)
-            if data:
-                print(data)
-                cl.send(data)
+        for cl in input_ready:
+            if cl == s:
+                client, address = s.accept()
+                #접속을 허가 했을때 표시할 내용
+                print("new user join")
+                input_list.append(client)
+                for socket_in_list in input_list:
+                    if socket_in_list != s and socket_in_list != cl:
+                        try:
+                            socket_in_list.send("new user join!")
+                        except Exception as e:
+                            socket_in_list.close()
+                            input_list.remove(socket_in_list)
             else:
-                cl.close()
-                input_list.remove(cl)
+                data = cl.recv(1024)
+                if data:
+                    print("data is come", data)
+                    for socket_in_list in input_list:
+                        if socket_in_list != s and socket_in_list != cl:
+                            try:
+                                socket_in_list.send(data)
+                            except Exception as e:
+                                print(e)
+                                socket_in_list.close()
+                                input_list.remove(socket_in_list)
+                                continue
+                    cl.send(data)
+                else:
+                    cl.close()
+                    input_list.remove(cl)
+    except KeyboardInterrupt
+        s.close()
 
 s.close()
