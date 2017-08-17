444eb05e7f130cd6b3f79d21a9c85b960d2722fa
net9keep <net9keep@naver.com>
2017-08-18 03:27:50 +0900

    [updata] make protocol (login, signup)

--- Client.py
+++ Client.py
@@ -7,34 +7,79 @@ import sys
 SERVER_IP = Server_Info.SERVER_IP
 SERVER_PORT = Server_Info.SERVER_PORT
 
-def prompt():
-    sys.stdout.write('<나> ')
-    sys.stdout.flush()
 
+class Client:
 
-client_socket = socket(AF_INET, SOCK_STREAM)
-try:
-    client_socket.connect((SERVER_IP,SERVER_PORT))
-except Exception as e:
-    print("서버에 접속하지 못했습니다.")
+    def __init__(self):
+        self.id = self.pw = ""
+        self.data = {}
+        self.choice = 0
+        self.client_socket = socket(AF_INET, SOCK_STREAM)
+        try:
+            self.client_socket.connect((SERVER_IP, SERVER_PORT))
+        except Exception as e:
+            print("서버에 접속하지 못했습니다.")
+            sys.exit()
+        print("서버에 접속했습니다")
+        self.login()
 
-while True:
-    try:
-        connection_list = [sys.stdin, client_socket]
-        input_ready, write_ready, except_ready = select(connection_list, [], [], 10)
-        for sock in input_ready:
-            if sock == client_socket:
-                data = sock.recv(1024)
-                if not data:
-                    print("연결이...?")
-                    prompt()
-                else:
-                    print(data)
-                    prompt()
-            else:
-                message = sys.stdin.readline()
-                client_socket.send(message.encode())
-                prompt()
-    except KeyboardInterrupt:
-        client_socket.close()
+    def login(self):
+        print("*--------------*")
+        print("1.login")
+        print("2.SignUp")
+        print("*--------------*")
+        self.choice = input("> ")
+        print(self.choice)
+        if self.choice == '1':
+            print("*--------------*")
+            self.id = input("id > ")
+            self.pw = input("pw > ")
+            print("*--------------*")
+        elif self.choice == '2':
+            print("*----signup----*")
+            self.id = input("id > ")
+            self.pw = input("pw > ")
+            print("*--------------*")
+        else:
+            print("wrong number")
+            self.login()
+        self.data = {"id": self.id, "pw": self.pw}
+        self.send_data()
 
+    def send_data(self):
+        while True:
+            try:
+                connection_list = [sys.stdin, self.client_socket]
+                input_ready, write_ready, except_ready = select(connection_list, [], [], 10)
+                for sock in input_ready:
+                    if sock == self.client_socket:
+                        data = sock.recv(1024)
+                        if not data:
+                            print("연결이...?")
+                            self.prompt()
+                        elif data == "SignUp Success":
+                            print(data)
+                            self.prompt()
+                        else:
+                            print(data)
+                            self.prompt()
+                            if self.data and self.choice == '1':
+                                message = "Login ID:{0} PW:{1}".format(self.data["id"], self.data["pw"]).encode()
+                                self.client_socket.send(message)
+                                self.choice = 0
+                            elif self.data and self.choice == '2':
+                                message = "SignUp ID:{0} PW:{1}".format(self.data["id"], self.data["pw"]).encode()
+                                self.client_socket.send(message)
+                                self.choice = 0
+                    else:
+                        message = sys.stdin.readline()
+                        self.client_socket.send(message.encode())
+                        self.prompt()
+            except KeyboardInterrupt:
+                self.client_socket.close()
+
+    def prompt(self):
+        sys.stdout.write('<나> ')
+        sys.stdout.flush()
+
+client = Client()
--- Server.py
+++ Server.py
@@ -3,48 +3,73 @@ import select
 import  Server_Info
 SERVER_IP = Server_Info.SERVER_IP
 SERVER_PORT = Server_Info.SERVER_PORT
+class Server:
+    def __init__(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((SERVER_IP,SERVER_PORT))
+        self.s.listen(10)
+        self.input_list = [self.s]
+        self.listen()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((SERVER_IP,SERVER_PORT))
-s.listen(10)
-input_list = [s]
+    def check_data(self, data):
+        #데이터 체크
+        login_type = {"Login":"","SignUp":""}
+        data = data.decode()
+        data_split = data.split()
+        print(data_split)
+        try:
+            login_type['Login'] = data_split.index('Login')
+        except ValueError:
+            login_type['SignUp'] = 0
 
-while input_list:
-    try:
-        input_ready, write_ready, except_ready = select.select(input_list, [], [])
+        if login_type['Login'] == 0 or login_type['SignUp'] == 0:
+            id = data_split[1].replace("ID:","")
+            pw = data_split[2].replace("PW:","")
+            print("id:{0} pw:{1}".format(id,pw))
+            return data
+        else:
+            return data
 
-        for cl in input_ready:
-            if cl == s:
-                client, address = s.accept()
-                #접속을 허가 했을때 표시할 내용
-                print("new user join")
-                input_list.append(client)
-                for socket_in_list in input_list:
-                    if socket_in_list != s and socket_in_list != cl:
-                        try:
-                            socket_in_list.send("new user join!".encode())
-                        except Exception as e:
-                            socket_in_list.close()
-                            input_list.remove(socket_in_list)
-            else:
-                data = cl.recv(1024)
-                if data:
-                    print("data is come", data)
-                    for socket_in_list in input_list:
-                        if socket_in_list != s and socket_in_list != cl:
-                            try:
-                                print("send data", data)
-                                socket_in_list.send(data)
-                            except Exception as e:
-                                print(e)
-                                socket_in_list.close()
-                                input_list.remove(socket_in_list)
-                                continue
-                    cl.send(data)
-                else:
-                    cl.close()
-                    input_list.remove(cl)
-    except KeyboardInterrupt:
-        s.close()
 
-s.close()
+    def listen(self):
+        while self.input_list:
+            try:
+                input_ready, write_ready, except_ready = select.select(self.input_list, [], [])
+
+                for cl in input_ready:
+                    if cl == self.s:
+                        client, address = self.s.accept()
+                        #접속을 허가 했을때 표시할 내용
+                        print("new user join")
+                        self.input_list.append(client)
+                        for socket_in_list in self.input_list:
+                            if socket_in_list != self.s and socket_in_list != cl:
+                                try:
+                                    socket_in_list.send("new user join!".encode())
+                                except Exception as e:
+                                    socket_in_list.close()
+                                    self.input_list.remove(socket_in_list)
+                    else:
+                        data = cl.recv(1024)
+                        data = self.check_data(data)
+                        if data:
+                            print("data is come", data)
+                            for socket_in_list in self.input_list:
+                                if socket_in_list != self.s and socket_in_list != cl:
+                                    try:
+                                        print("send data", data)
+                                        socket_in_list.send(data.encode())
+                                    except Exception as e:
+                                        print(e)
+                                        socket_in_list.close()
+                                        self.input_list.remove(socket_in_list)
+                                        continue
+                            cl.send(data.encode())
+                        else:
+                            cl.close()
+                            self.input_list.remove(cl)
+            except KeyboardInterrupt:
+                self.s.close()
+        self.s.close()
+
+server = Server()
