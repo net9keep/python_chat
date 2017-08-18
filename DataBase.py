88a98eae30b89579f0e3dab03d2bdd8210ecf710
net9keep <net9keep@naver.com>
2017-08-18 16:54:00 +0900

    [updata] basic database connection

--- DataBase.py
+++ DataBase.py
@@ -1,1 +1,21 @@
 #database
+
+import sqlite3
+
+class DataBase:
+
+    def __init__(self):
+        self.conn_db = ""
+        self.cur = ""
+
+    def connect_db(self):
+        self.conn_db = sqlite3.connect("user.db")
+        self.find_user()
+
+    def find_user(self):
+        with self.conn_db:
+            self.cur = self.conn_db.cursor()
+            query_message = ""
+            self.cur.execut(query_message)
+            rows = self.cur.fetchall()
+            print(rows)
