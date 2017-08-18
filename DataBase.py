021308a30dc8ebb5d3ccead325fc6a8e0626f2e1
함태영 <tae0code@gmail.com>
2017-08-18 18:34:34 +0900

    [update, add] update : Database, sign up, and login function done add : user datebase add

--- DataBase.py
+++ DataBase.py
@@ -10,12 +10,43 @@ class DataBase:
 
     def connect_db(self):
         self.conn_db = sqlite3.connect("user.db")
-        self.find_user()
 
-    def find_user(self):
+    def find_user(self, id, pw):
         with self.conn_db:
             self.cur = self.conn_db.cursor()
-            query_message = ""
-            self.cur.execut(query_message)
+            if id and pw:
+                query_message = "select id, pw from user where id='" + id + "' and pw='" + pw + "'"
+            elif id:
+                query_message = "select id, pw from user where id='" + id + "'"
+
+            self.cur.execute(query_message)
             rows = self.cur.fetchall()
-            print(rows)
+            return rows
+
+    def login(self, id, pw):
+        self.connect_db()
+        result = self.find_user(id, pw)
+        if result:
+            return True
+        else:
+            return False
+
+    def sign_up(self, id, pw):
+        self.connect_db()
+        result_id = self.find_user(id, pw="")
+        if result_id:
+            #TODO write signUp false
+            return False
+        else:
+            #TODO write signUp success
+            with self.conn_db:
+                self.cur = self.conn_db.cursor()
+                query_message = "insert into user (id, pw) values ('" + id + "', '" + pw + "')"
+                self.cur.execute(query_message)
+            result = self.find_user(id,pw)
+            if result:
+                return True
+            else:
+                return False
+test = DataBase()
+test.connect_db()
